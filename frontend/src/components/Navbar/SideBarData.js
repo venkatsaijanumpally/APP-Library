32a13e4772e046d21fefc5db1315ac632bf00180
Madhava5 <50517164+Madhava5@users.noreply.github.com>
2022-11-14 00:16:26 -0500

    Add files via upload

---
+++ frontend/src/components/Navbar/SideBarData.js
@@ -0,0 +1,57 @@
+import React from 'react';
+import * as FaIcons from 'react-icons/fa';
+import * as AiIcons from 'react-icons/ai';
+import * as IoIcons from 'react-icons/io';
+
+export const SidebarData = [
+  {
+    title: 'Students Info',
+    path: '/',
+    cName: 'nav-text'
+  },
+  {
+    title: 'Add Student',
+    path: '/students',
+    cName: 'nav-text'
+  },
+  {
+    title: 'Book Borrow',
+    path: '/books',
+    cName: 'nav-text'
+  },
+  {
+    title: 'Create Book',
+    path: '/createbook',
+    cName: 'nav-text'
+  },
+  {
+    title: 'Pricing',
+    path: '/pricing',
+    cName: 'nav-text'
+  },
+  {
+    title: 'Student Borrow',
+    path: '/studentBorrow',
+    cName: 'nav-text'
+  },
+  {
+    title: 'Books Info',
+    path: '/bookDetails',
+    cName: 'nav-text'
+  },
+  {
+    title: 'Delete Student',
+    path: '/deleteStudent',
+    cName: 'nav-text'
+  },
+  {
+    title: 'Delete Borrow',
+    path: '/deleteStudentBorrow',
+    cName: 'nav-text'
+  },
+  {
+    title: 'Unreturned records',
+    path: '/deniedStudents',
+    cName: 'nav-text'
+  }
+];
